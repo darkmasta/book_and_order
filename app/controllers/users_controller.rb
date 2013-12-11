5f3eaa175659d3da6797bbf29a1ec8c792f567cb
darkmasta <alperen_bagci@hotmail.com>
2013-12-11 05:46:04 +0200

    Make devise authentication system

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def set_user
+      @user = current_user
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+end
+
